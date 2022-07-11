dd62976df30333558bcd270209d3f3dcaa014d63
酒酒 <2188484122@qq.com>
2022-07-11 18:20:37 +0800

    登录完成token持久化

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,14 +1,29 @@
 import { login } from '@/api/user'
-const state = {}
-const mutations = {}
+import { Message } from 'element-ui'
+// token持久化使用cookie
+import { setToken, getToken } from '@/utils/auth'
+const state = {
+  token: getToken() || ''
+}
+const mutations = {
+  setToken (state, token) {
+    state.token = token
+  }
+}
 const actions = {
   // 登录
   async login (context, data) {
     try {
       const res = await login(data)
       console.log(res)
+      if (res.data.success) {
+        context.commit('setToken', res.data.data)
+        setToken(res.data.data)
+      } else {
+        Message.error(res.data.message)
+      }
     } catch (err) {
-      console.log(err)
+      Message.error('登录失败，请重试')
     }
   }
 }
